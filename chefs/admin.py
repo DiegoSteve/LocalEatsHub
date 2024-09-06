256a5296aed70b44677f1d49280c6582d90fe649
DiegoSteve <diegosteve123321@gmail.com>
2024-09-05 19:52:15 -0600

    Create the chef model and 'make' file for delete migrations

--- chefs/admin.py
+++ chefs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Chef
 
+admin.site.register(Chef)
 # Register your models here.
--- chefs/models.py
+++ chefs/models.py
@@ -1,6 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Chef(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=150, unique=True)
+    profile_photo = models.ImageField(upload_to="chefs/", default="chefs/avatar.png")
+    phone = models.CharField(max_length=20)  # Usar CharField para el número de teléfono
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "chefs"
+
+    def __str__(self): 
+        return str(self.name)
