e14d8560c259a3c42ddf76d12e93b6dab71cb246
DiegoSteve <diegosteve123321@gmail.com>
2024-09-06 20:44:57 -0600

    created the rating model

---
+++ ratings/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from dishes.models import Dish
+from students.models import Student
+
+
+
+# Create your models here.
+class Rating(models.Model):
+    CHOICES_RATING = (
+        ("1","1"),
+        ("2","2"),
+        ("3","3"),
+        ("4","4"),
+        ("5","5"),
+    )
+
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name="ratings")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="students")
+    raiting = models.CharField(max_length=1,choices=CHOICES_RATING, default="5")
+    review_text = models.models.TextField(blank=True, default="Sin descripciones")
