50f3830e3635b4b5d485e9ff0d8c3b737980b270
Rene Silva <rene.scre@gmail.com>
2024-09-07 18:50:47 -0600

    rating model updated

--- ratings/models.py
+++ ratings/models.py
@@ -3,18 +3,19 @@ from dishes.models import Dish
 from students.models import Student
 
 
-
 # Create your models here.
 class Rating(models.Model):
     CHOICES_RATING = (
-        ("1","1"),
-        ("2","2"),
-        ("3","3"),
-        ("4","4"),
-        ("5","5"),
+        ("1", "1"),
+        ("2", "2"),
+        ("3", "3"),
+        ("4", "4"),
+        ("5", "5"),
     )
 
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name="ratings")
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="students")
-    raiting = models.CharField(max_length=1,choices=CHOICES_RATING, default="5")
-    review_text = models.models.TextField(blank=True, default="Sin descripciones")
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="students"
+    )
+    raiting = models.CharField(max_length=1, choices=CHOICES_RATING, default="5")
+    review_text = models.TextField(blank=True, default="Sin descripciones")
